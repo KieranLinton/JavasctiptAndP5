809b6679b3beee7fc3bb625c67910ae7b103a751
soggyDoughnut <proton5@live.com>
2019-02-08 00:38:11 +0100

    making the code more understandable for when I forget tomorrow

--- sketch.js
+++ sketch.js
@@ -1,33 +1,33 @@
 /* eslint-env es6 */
 
 class Boxthing {
-    constructor(){
-        this.currentXY = {
+    constructor(){           //all class variables you want to access ouytside the class have to go in the constructor
+        this.currentXY = {      //creating a tree called currentCY that has, the x and y, set to null for now
             X: null,
             y:  null
         }
-        this.mooo = "hey you"
+        this.mooo = "hey you"     //and another variable, which in this case is a string
     }
     updateColor(positionx, positiony){
         
-        noStroke();
-        fill(random(255), random(255), random(255));
-        ellipse(positionx, positiony, 50);
+        noStroke();                                   //dont want a border on the circle
+        fill(random(255), random(255), random(255)); //setting fill color to some random rgb
+        ellipse(positionx, positiony, 50);             //after setting up all that, we now draw a circle
         this.currentXY.X = positionx;
-        this.currentXY.y = positiony;
+        this.currentXY.y = positiony;   // and we set the classes variables to out current position
     }
 }
 
 
 
-var BOX = new Boxthing;
-var BOX2 = new Boxthing;
+var BOX = new Boxthing;         //creating a new object of class Boxthing
+var BOX2 = new Boxthing;         //and another
 
 
 function setup() {
-    createCanvas(300,300);
+    createCanvas(300,300);    //setting the canvas
  
-    background(0,0,0); 
+    background(0,0,0);        //making the backround black
  
 }
 
@@ -38,15 +38,16 @@ function draw() {
 }
 
 function ChangeColors(){
-    var speeed = parseInt(document.getElementById("inputZone").value);
+    var speeed = parseInt(document.getElementById("inputZone").value);   //setting speed value to the input from html
     
-    background(0,0,0);
+    background(0,0,0);                                     //refreshing the background, draws over the brevious draws, so esencially cleans the canvas
     
-    BOX.updateColor(50,BOX.currentXY.y + speeed);   
-    BOX2.updateColor(BOX.currentXY.X + 100, BOX.currentXY.y);
+    BOX.updateColor(50,BOX.currentXY.y + speeed);               //Activating the function attatched to the object that colors and draws it, and sets its position parameters to x= 50 and Y= to its current one + the speed.
+    BOX2.updateColor(BOX.currentXY.X + 100, BOX.currentXY.y);   //Activating the function attatched to the object that colors and draws it,
+                                                                //and sets its position parameters to x = the first box + 100, and y = the same as first box(BOX)
     
     print(BOX2.mooo);
-    print("your current speed is: " , speeed);
+    print("your current speed is: " , speeed);       //some debug
 
 }
 
